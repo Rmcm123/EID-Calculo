3814a627957a16ccbef9ca806aed385f76d28dcf
Rmcm123 <tenatohola@gmail.com>
2026-05-22 18:32:50 -0400

    Actualización en modulo_rut.py

--- modulo_rut.py
+++ modulo_rut.py
@@ -25,10 +25,43 @@ class DigitoVerificador:
         }
     
     @staticmethod
-    def _determinar_dv(resultado_resta):
+    def _determinar_dv(resultado_resta: int) -> str:
         if resultado_resta == 11:
-            return 0
+            return "0"
         elif resultado_resta == 10:
             return "K"
         return str(resultado_resta)
     
+class AnalizadorRut:
+    def __init__(self, rut_completo: str):
+        self.rut_completo = rut_completo
+        self.rut_limpio = self._limpiar_rut(rut_completo)
+        self.cuerpo, self.dv_ingresado = self._split_rut()
+        self.validar_cuerpo()
+    
+    def _limpiar_rut(self, rut: str) -> str:
+        return rut.replace(".", "").replace("-", "").upper()
+    
+    def _split_rut(self) -> tuple[str, str]:
+        if len(self.rut_limpio) < 6:
+            raise ValueError("El RUT ingresado es demasiado corto.")
+        elif len(self.rut_limpio) > 9:
+            raise ValueError("El RUT ingresado es demasiado largo.")
+        return self.rut_limpio[:-1].zfill(8), self.rut_limpio[-1]
+
+    def validar_cuerpo(self) -> None:
+        if not self.cuerpo.isdigit():
+            raise ValueError("El cuerpo del RUT debe contener solo dígitos.")
+    
+    def obtener_digitos(self) -> list[int]:
+        return [int(d) for d in self.cuerpo]
+    
+    def es_valido(self, resultado_calculo: dict) -> bool:
+        return resultado_calculo["dv_esperado"] == self.dv_ingresado
+    
+    def variable_v(self, dv_esperado: str) -> int:
+        if dv_esperado == "K":
+            return 10
+        elif dv_esperado == "0":
+            return 11
+        return int(dv_esperado)
